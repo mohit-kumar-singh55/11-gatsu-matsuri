11b81b1055dcb4a8ecf4532dcfd76883a08b6525
mohit-kumar-singh55 <mohitkumarsingh12344@gmail.com>
2025-10-27 09:57:56 +0900

    Update: 制限時間延長アイテムは後で

---
+++ Assets/Scripts/Items/TimeLimitExtension.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class TimeLimitExtension : MonoBehaviour, Interactable
+{
+    [Tooltip("時間制限の延長時間 (秒)")]
+    [SerializeField] private float increaseTimeLimitBy = 20f;
+
+    public void OnInteract(PlayerController player = null)
+    {
+        if (player == null) return;
+
+        // TODO: chagne the time limit
+
+        // player.ChangeBothSpeedForSomeTime(multipleOfSpeed, speedUpDuration);
+
+        // Destroy(gameObject);
+    }
+}
